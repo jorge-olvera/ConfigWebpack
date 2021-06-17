9e021b7a68990f611035127e8e3ee0054183f003
jorge-olvera <jorgeboge1998@gmail.com>
2021-06-17 16:16:36 -0500

    Primer commit de las config y ejemp de webpack

---
+++ src/index.js
@@ -0,0 +1,8 @@
+//las importaciones de modulos no es necesario escribir la extension de .js
+// ya que por defecto buscará en el directorio los archivos que cumplan con dicha extensión
+
+import {saludar} from './js/componentes';
+import './styles.css';
+const nombre = 'Jorge'; 
+
+saludar(nombre);
